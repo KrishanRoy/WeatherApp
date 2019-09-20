3c9bf0ca5e75a9406580c08490e7eb8880c82731
Krishan Roy <krishanroy@pursuit.org>
2019-09-19 20:20:21 -0400

    add a singleton class to make a single Retrofit instance

---
+++ app/src/main/java/org/krishan/weatherapp/network/RetrofitSingleton.java
@@ -0,0 +1,28 @@
+package org.krishan.weatherapp.network;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitSingleton {
+    private static final String BASE_URL = "https://api.darksky.net/";
+
+    private static Retrofit instance;
+    public static Retrofit getInstance() {
+        if (instance == null) {
+            instance = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return instance;
+    }
+
+    /**
+     * private constructor to prevent this class to make further instances
+     */
+    private RetrofitSingleton() {
+    }
+}
