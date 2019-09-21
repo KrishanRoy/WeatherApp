8f5ae754f5605e80fc408a691dd3d91cd379cd42
Krishan Roy <krishanroy@pursuit.org>
2019-09-20 20:08:53 -0400

    annotate WeatherDatabase and add abstract method weatherDao in database class

--- app/src/main/java/org/krishan/weatherapp/db/WeatherDatabase.java
+++ app/src/main/java/org/krishan/weatherapp/db/WeatherDatabase.java
@@ -1,4 +1,13 @@
 package org.krishan.weatherapp.db;
 
+import androidx.room.Database;
+
+import org.krishan.weatherapp.model.ForecastModel;
+
+@Database(entities = {ForecastModel.class}, version = 1, exportSchema = false)
 public abstract class WeatherDatabase {
+    public abstract WeatherDao weatherDao();
+
+    private static WeatherDatabase INSTANCE;
+
 }
