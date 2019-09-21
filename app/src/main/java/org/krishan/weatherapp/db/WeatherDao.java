48dc0a00942d7c1c5773bf1f0ab91e0b2795e93a
Krishan Roy <krishanroy@pursuit.org>
2019-09-20 20:06:15 -0400

    add two methods in the dao class to Query and insert into Room db

--- app/src/main/java/org/krishan/weatherapp/db/WeatherDao.java
+++ app/src/main/java/org/krishan/weatherapp/db/WeatherDao.java
@@ -1,4 +1,16 @@
 package org.krishan.weatherapp.db;
 
-public class WeatherDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import org.krishan.weatherapp.model.ForecastModel;
+
+public interface WeatherDao {
+    @Query("SELECT * FROM ForecastModel")
+    LiveData<ForecastModel> getForecastModel();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertWeatherInfo(ForecastModel forecastModels);
 }
